068d590a86496b153d11aefc73e3cc12e6fc4a00
SayedaRehmat <bc190408541@vu.edu.pk>
2025-07-05 01:36:24 -0700

    Update main.py

--- backend-api/app/main.py
+++ backend-api/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from app.endpoints import health
+from app.endpoints import health, predict  # add predict
 
 app = FastAPI()
 app.include_router(health.router)
+app.include_router(predict.router)  # include new router
